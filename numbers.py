ad7572acf61d36c730244c070957e3c8195b31e5
idham <idhamkholid965@gmail.com>
2019-08-23 13:17:37 +0700

    add numbers.py

---
+++ numbers.py
@@ -0,0 +1,18 @@
+a = 5
+b = 3
+c = 4.0
+
+# penjumlahan
+print("penjumlahan a + b = ", a + b)
+
+# pengurangan
+print("penjumlahan a - b = ", a - b)
+
+# perkalian
+print("penjumlahan a * b = ", a * b)
+
+# pembagian
+print("penjumlahan a / b = ", a / b)
+
+# perpangkatan
+print("penjumlahan a ** b = ", a ** b)
