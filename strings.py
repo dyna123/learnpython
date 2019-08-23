c3d66763761f4eff679450e1710f5225de1e0113
idham <idhamkholid965@gmail.com>
2019-08-23 13:43:03 +0700

    add strings

---
+++ strings.py
@@ -0,0 +1,21 @@
+text1 = 'jalan - jalan di hari minggu'
+
+text2 = 'jalan - jalan di hari jum\'at'
+
+text3 = "jalan - jalan di hari jum'at"
+
+text4 = 'idham berkata, "kemarin kemana bro ?"'
+
+text5 = "k menjawab, \"jalan jalan bro\" "
+
+text6 = 'idham berkata, "kemarin kemana bro ?" \nk menjawab, \"jalan jalan bro\"  '
+
+text7 = """
+idham berkata, "kemarin kemana bro ?"
+k menjawab, \"jalan jalan bro\"
+"""
+
+text8 = r'C:\nyoto'  # raw string
+
+print(text8)
+print(5*"wk")
