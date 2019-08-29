cbb396fa10fb4563dab6c7cd0b05b779e62b5547
idham <idhamkholid965@gmail.com>
2019-08-29 16:55:16 +0700

    add if else

---
+++ part5 if elif else/if elif else.py
@@ -0,0 +1,23 @@
+nilai = 50
+
+if nilai == 75:
+    print('Nilai anda:', nilai)
+
+if nilai is 60:
+    print('Nilai anda:', nilai)
+
+if nilai is not 60:
+    print('Nilai anda bukan: 60')
+
+nilai = 65
+
+if 80 <= nilai <= 100:
+    print("nilai anda adalah A")
+elif 70 <= nilai <= 80:
+    print("nilai anda adalah B")
+elif 60 <= nilai <= 70:
+    print("nilai anda adalah C")
+elif 50 <= nilai <= 60:
+    print("nilai anda adalah D, Lakukan perbaikan")
+else:
+    print("maaf anda tidak lulus")
